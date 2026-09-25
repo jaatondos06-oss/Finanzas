a6f0895b2cca7d852a043eb4f3599a259e2100b4
jaatondos06-oss <jaatondos06@gmail.com>
2026-09-24 22:15:09 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Guarda la app para que abra aunque no haya señal. Siempre intenta la versión más nueva primero.
-const C='misfinanzas-v1';
+const C='misfinanzas-v2';
 const FILES=['./','index.html','manifest.json','icon-180.png','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
